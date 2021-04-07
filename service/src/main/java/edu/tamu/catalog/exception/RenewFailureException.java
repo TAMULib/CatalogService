f39b7b5dbd7add77574dfa8a304cfd56cee64d1a
William Welling <wwelling@library.tamu.edu>
2021-04-07 12:20:13 -0500

    Issue 146: handle renew 422 exception (#147)
    * handle renew 422 exception
    
    * remove stack trace

---
+++ service/src/main/java/edu/tamu/catalog/exception/RenewFailureException.java
@@ -0,0 +1,11 @@
+package edu.tamu.catalog.exception;
+
+public class RenewFailureException extends Exception {
+
+  private static final long serialVersionUID = 502975922915583676L;
+
+  public RenewFailureException(String message) {
+    super(message);
+  }
+
+}
